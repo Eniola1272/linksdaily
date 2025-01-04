9b9823b8eb14fe5f45ebe17f611d46465145f226
Eniola Aderounmu <date1272@gmail.com>
2025-01-04 21:09:44 +0100

    day 44

--- Components/Welcome.js
+++ Components/Welcome.js
@@ -5,7 +5,7 @@ import { Text, View } from 'react-native';
 
 const Welcome = ({name}) => (
   <View>
-    <Text>Hello {name}</Text>
+    <Text>Hello {name}, Welcome to your dashboard</Text>
   </View>
 )
 
